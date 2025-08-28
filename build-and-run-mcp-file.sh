4b1bcaaff2b06242ed3463990dfcc2e6fbc4eb76
hpeters83 <hugo.peters@cloud.com>
2025-08-28 18:02:20 +0200

    DevHubUpdate

--- build-and-run-mcp-file.sh
+++ build-and-run-mcp-file.sh
@@ -6,16 +6,20 @@ echo "you passed me $@"
 echo $1
 cd $1
 ls
+echo TOKEN: $2
+
 
 # TODO: Pass this in as argument
-source token.sh
+# source token.sh
+platform_token=$2
 
-echo "MCP SEVER NAME: $2"
+# echo "MCP SEVER NAME: $2"
 
 # FILE="TestMCP.flogo"
 # FILE="TestMCP.json"
 # FILE="Customer_MCP_Client.flogo"
-FILE="Platform_MCP_Server.flogo"
+# FILE="Platform_MCP_Server.flogo"
+FILE="MCP_Server.flogo"
 # SPEC_FILE="simple_spec.json"
 SPEC_FILE="control-plane-api-1.9.json"
 # CustomerSwaggerClient.json
@@ -48,18 +52,20 @@ fdev sc property API_Endpoint "https://tibcopm.us-west.my.tibco.com" -f $FILE
 # Add this for deployment
 fdev sc any metadata.endpoints --jsonValue "[{\"protocol\": \"http\",\"port\": \"$MCP_PORT\",\"title\": \"MCPTrigger\",\"type\": \"public\"}]" -f $FILE
 
-PROMPT_FLOW=prompt_flow
+PROMPT_FLOW="prompt_flow"
+MCP_HANDLER="UserFinder"
+MCP_HANDLER_DESCRIPTION="Find your user"
+PROMPT="Find the user with id 1234"
 
 fdev cf $PROMPT_FLOW -f $FILE
-fdev cth $PROMPT_FLOW MCPTrigger --mcpHandlerType Prompt --mcpHandlerName 'UserFinder' --mcpHandlerDescription 'Find your user' -f $FILE
+fdev cth $PROMPT_FLOW MCPTrigger --mcpHandlerType Prompt --mcpHandlerName "$MCP_HANDLER" --mcpHandlerDescription "$MCP_HANDLER_DESCRIPTION" -f $FILE
 fdev aa $PROMPT_FLOW set_prompt actreply -f $FILE
 fdev sc flow $PROMPT_FLOW.data.metadata --jsonFile prompt-interface.json  -f $FILE
 fdev sc activity $PROMPT_FLOW.set_prompt.input.reply.mapping.promptResult.messages[0].role user -f $FILE
-fdev sc activity $PROMPT_FLOW.set_prompt.input.reply.mapping.promptResult.messages[0].content "Find the user with id 1234" -f $FILE
+fdev sc activity $PROMPT_FLOW.set_prompt.input.reply.mapping.promptResult.messages[0].content "$PROMPT" -f $FILE
 fdev sc activity $PROMPT_FLOW.set_prompt.schemas --jsonFile prompt-reply-interface.json -f $FILE
 
-
-flogobuild build-exe -f $FILE
+# flogobuild build-exe -f $FILE
 
 # flogobuild build-tp-deployment -f $FILE -o ./flogo-mcp-server.zip
 flogobuild build-tp-deployment -f $FILE -o ./
